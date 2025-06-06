5146ff9738825ebba7de4fe14fb2fe779d275373
Cloris-la <cloris.f.chen@gmail.com>
2025-06-06 16:12:48 +0200

    "First time to summit main.py"

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import pandas as pd
 from utils.openspace import Openspace
 
-#
+# load the name of people（each line is a name and no title）
+df = pd.read_csv("colleagues.csv", header=None)
+names = df[0].dropna().tolist()
+
+# Init Openspace（for example 6 tables，4 seats for each table）
+openspace = Openspace(number_of_tables=6, seats_per_table=4)
+
+# assign the seat
+openspace.organize(names)
+
+# show the distribution
+openspace.display()
+
+# Save all distribution as a excel file
+openspace.store("result.xlsx")
+
